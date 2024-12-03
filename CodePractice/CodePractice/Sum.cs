fdda2ecf76cf325ae86e749df1be439437c674f9
Dholon AKter <dholonakter721@gmail.com>
2024-12-03 12:37:57 +0100

    calculating miliseonds

--- CodePractice/CodePractice/Sum.cs
+++ CodePractice/CodePractice/Sum.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Xml.Linq;
+using System.Drawing;
 
 namespace CodePractice
 {
@@ -54,9 +55,40 @@ namespace CodePractice
 				///Create a function which answers the question "Are you playing banjo?".If your name starts with the letter "R" or lower case "r", you are playing banjo!The function takes a name as its only argument, and returns one of the following strings:name + " plays banjo" 
 				////name + " does not play banjo"
 				///Names given are always valid strings.
-				///
-
+				//
 			}
 
+
+	/// <summary>
+	/// Clock shows h hours, m minutes and s seconds after midnight.
+
+       //	Your task is to write a function which returns the time since midnight in milliseconds.
+
+       //	Example:
+     //h = 0
+    //m = 1
+//s = 1
+
+//result = 61000
+//Input constraints:
+
+//0 <= h <= 23
+//0 <= m <= 59
+//0 <= s <= 59
+
+	/// </summary>
+	public static class Clock
+	{
+		public static int Past(int h, int m, int s) 
+		{
+			if(h>=0 && h<=23 && m>=0 && m<=59 && s>=0 && s <= 59)
+			{
+				return (h * 3600000) + (m * 6000) + (s * 1000);
+
+			}
+			throw new ArgumentException("Invalid input for hours, minutes, or seconds.");
+
+		}
+	}
 
 }
